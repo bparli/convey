07e12359b6025d929dd9763eef719fe70439043e
Ben Parli <ben.parli@creditkarma.com>
2020-01-08 17:20:53 -0800

    use tokio rwlock

--- src/proxy/backend.rs
+++ src/proxy/backend.rs
@@ -1,13 +1,14 @@
-use std::net::SocketAddr;
-use std::collections::HashMap;
-use std::net::TcpStream;
-use std::sync::{Arc, RwLock};
 use crate::stats::StatsMssg;
-use std::sync::mpsc::Sender;
 use rand::distributions::WeightedIndex;
 use rand::prelude::*;
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::net::TcpStream;
+use std::sync::mpsc::Sender;
+use std::sync::Arc;
 use std::time;
 use std::time::Duration;
+use tokio::sync::RwLock;
 use tokio::task;
 
 #[derive(Debug, Clone)]
@@ -44,28 +45,48 @@ impl ServerPool {
             let mut server_weight: u16 = 1;
             match weight {
                 Some(w) => server_weight = *w,
-                None => {},
+                None => {}
             }
             weighted_backend_servers.push(*server);
             if tcp_health_check(*server) {
                 // server health check successful
                 weights.push(server_weight);
-                backend_servers.insert(*server, Wrr{healthy: true, weight: server_weight, weights_index: index_count});
+                backend_servers.insert(
+                    *server,
+                    Wrr {
+                        healthy: true,
+                        weight: server_weight,
+                        weights_index: index_count,
+                    },
+                );
             } else {
                 // server health check not successful. set weight to 0
                 weights.push(0);
-                backend_servers.insert(*server, Wrr{healthy: false, weight: server_weight, weights_index: index_count});
+                backend_servers.insert(
+                    *server,
+                    Wrr {
+                        healthy: false,
+                        weight: server_weight,
+                        weights_index: index_count,
+                    },
+                );
             }
 
             index_count += 1;
         }
-        debug!("Created weighted backend server pool: {:?}", backend_servers);
+        debug!(
+            "Created weighted backend server pool: {:?}",
+            backend_servers
+        );
 
         let mut servers_dist = None;
         match WeightedIndex::new(weights.clone()) {
             Ok(dist) => servers_dist = Some(dist),
             Err(e) => {
-                error!("Unable to set weighted distribution for server pools: {:?}", e);
+                error!(
+                    "Unable to set weighted distribution for server pools: {:?}",
+                    e
+                );
                 servers_dist = None;
             }
         }
@@ -80,7 +101,11 @@ impl ServerPool {
 }
 
 impl Backend {
-    pub fn new(name: String, servers: HashMap<SocketAddr, Option<u16>>, health_check_interval: u64) -> Backend {
+    pub fn new(
+        name: String,
+        servers: HashMap<SocketAddr, Option<u16>>,
+        health_check_interval: u64,
+    ) -> Backend {
         Backend {
             name: name,
             servers: Arc::new(RwLock::new(ServerPool::new_servers(servers))),
@@ -88,8 +113,8 @@ impl Backend {
         }
     }
 
-    fn update_backends_health(&self, updates: &HashMap<SocketAddr, bool>) {
-        let mut srvs = self.servers.write().unwrap();
+    async fn update_backends_health(&self, updates: &HashMap<SocketAddr, bool>) {
+        let mut srvs = self.servers.write().await;
         let mut wgts = srvs.weights.clone();
 
         let mut reset_dist = false;
@@ -117,25 +142,18 @@ impl Backend {
     }
 }
 
-pub fn get_next(backend: Arc<Backend>) -> Option<SocketAddr> {
-    match backend.servers.read() {
-        Ok(srvs) => {
-            if let Some(dist) = &srvs.dist {
-                if let Some(addr) = srvs.weighted_servers.get(dist.sample(&mut thread_rng())) {
-                    Some(*addr)
-                } else{
-                    error!("Backend {} unhealthy; Unable to schedule", backend.name);
-                    None
-                }
-            } else {
-                error!("Backend {} unhealthy; Unable to schedule", backend.name);
-                None
-            }
-        }
-        Err(e) => {
-            error!("Unable to schedule backend: {:?}", e);
+pub async fn get_next(backend: Arc<Backend>) -> Option<SocketAddr> {
+    let srvs = backend.servers.read().await;
+    if let Some(dist) = &srvs.dist {
+        if let Some(addr) = srvs.weighted_servers.get(dist.sample(&mut thread_rng())) {
+            Some(*addr)
+        } else {
+            error!("Backend {} unhealthy; Unable to schedule", backend.name);
             None
         }
+    } else {
+        error!("Backend {} unhealthy; Unable to schedule", backend.name);
+        None
     }
 }
 
@@ -147,32 +165,43 @@ fn tcp_health_check(server: SocketAddr) -> bool {
     }
 }
 
-pub async fn run_health_checker(back: Arc<Backend>, sender: Sender<StatsMssg>) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn run_health_checker(
+    back: Arc<Backend>,
+    sender: Sender<StatsMssg>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let mut interval = tokio::time::interval(Duration::from_secs(back.health_check_interval));
 
     loop {
         let health_sender = sender.clone();
         let health_back = back.clone();
-        debug!("Running backend health checker{:?}, {:?}", health_back, interval);
+        debug!(
+            "Running backend health checker{:?}, {:?}",
+            health_back, interval
+        );
 
-        task::spawn_blocking(|| {
-            health_checker(health_back, health_sender)
-        }).await?;
-
+        health_checker(health_back, health_sender).await?;
         interval.tick().await;
     }
 }
 
-fn health_checker(backend: Arc<Backend>, sender: Sender<StatsMssg>) {
+async fn health_checker(
+    backend: Arc<Backend>,
+    sender: Sender<StatsMssg>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let mut backend_status = HashMap::new();
     let mut update = false;
     // limit scope of read lock
     {
-        let srvs = backend.servers.read().unwrap();
+        let srvs = backend.servers.read().await;
         for (server, status) in srvs.servers_map.iter() {
-            let res = tcp_health_check(*server);
+            let thread_server = server.clone();
+            let join = task::spawn_blocking(move || tcp_health_check(thread_server));
+            let res = join.await?;
             if res != status.healthy {
-                info!("Server {} status has changed from {} to {}.  Updating stats and backend", server, status.healthy, res);
+                info!(
+                    "Server {} status has changed from {} to {}.  Updating stats and backend",
+                    server, status.healthy, res
+                );
                 update = true;
             }
             backend_status.insert(server.clone(), res);
@@ -181,10 +210,11 @@ fn health_checker(backend: Arc<Backend>, sender: Sender<StatsMssg>) {
 
     // update_backends_health uses the write lock so only call it when absolutely necessary
     if update {
-        backend.update_backends_health(&backend_status);
+        backend.update_backends_health(&backend_status).await;
     }
 
     send_status(backend.name.clone(), backend_status, sender);
+    Ok(())
 }
 
 fn send_status(name: String, updates: HashMap<SocketAddr, bool>, sender: Sender<StatsMssg>) {
@@ -192,33 +222,35 @@ fn send_status(name: String, updates: HashMap<SocketAddr, bool>, sender: Sender<
     for (srv, healthy) in updates {
         servers.insert(srv.to_string(), healthy);
     }
-    let mssg = StatsMssg{frontend: None,
-                        backend: name.clone(),
-                        connections: 0,
-                        bytes_tx: 0,
-                        bytes_rx: 0,
-                        servers: Some(servers)};
+    let mssg = StatsMssg {
+        frontend: None,
+        backend: name.clone(),
+        connections: 0,
+        bytes_tx: 0,
+        bytes_rx: 0,
+        servers: Some(servers),
+    };
     match sender.send(mssg) {
-        Ok(_) => {},
-        Err(e) => error!("Error sending stats message on channel: {}", e)
+        Ok(_) => {}
+        Err(e) => error!("Error sending stats message on channel: {}", e),
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::str::FromStr;
     use std::net::TcpListener;
+    use std::str::FromStr;
     use std::{thread, time};
+    use tokio::runtime;
 
     #[test]
     fn test_new_servers() {
-        thread::spawn( ||{
+        thread::spawn(|| {
             let listener = TcpListener::bind("127.0.0.1:8070").unwrap();
             match listener.accept() {
-                Ok((_socket, _addr)) => {},
-                Err(_e) => {},
+                Ok((_socket, _addr)) => {}
+                Err(_e) => {}
             }
         });
 
@@ -237,18 +269,21 @@ mod tests {
         assert!(test_pool.weights.contains(&test_weight));
         assert_eq!(test_pool.weights.len(), 2);
 
-        let test_srv = test_pool.servers_map.get(&FromStr::from_str("127.0.0.1:8071").unwrap()).unwrap();
+        let test_srv = test_pool
+            .servers_map
+            .get(&FromStr::from_str("127.0.0.1:8071").unwrap())
+            .unwrap();
         assert_eq!(test_srv.healthy, false);
         assert_eq!(test_srv.weight, 100);
     }
 
     #[test]
     fn test_backend() {
-        thread::spawn( ||{
+        thread::spawn(|| {
             let listener = TcpListener::bind("127.0.0.1:8090").unwrap();
             match listener.accept() {
-                Ok((_socket, _addr)) => {},
-                Err(_e) => {},
+                Ok((_socket, _addr)) => {}
+                Err(_e) => {}
             }
         });
 
@@ -264,10 +299,15 @@ mod tests {
 
         let mut test_updates = HashMap::new();
         test_updates.insert(FromStr::from_str("127.0.0.1:8091").unwrap(), true);
-        test_bck.update_backends_health(&test_updates);
 
-        let test_pool = test_bck.servers.read().unwrap();
+        let mut rt = runtime::Builder::new()
+            .basic_scheduler()
+            .enable_all()
+            .build()
+            .unwrap();
 
+        rt.block_on(test_bck.update_backends_health(&test_updates));
+        let test_pool = rt.block_on(test_bck.servers.read());
         let mut test_weight: u16 = 100;
         assert!(test_pool.weights.contains(&test_weight));
         test_weight = 1;
@@ -276,11 +316,11 @@ mod tests {
 
     #[test]
     fn test_get_next() {
-        thread::spawn( ||{
+        thread::spawn(|| {
             let listener = TcpListener::bind("127.0.0.1:8082").unwrap();
             match listener.accept() {
-                Ok((_socket, _addr)) => {},
-                Err(_e) => {},
+                Ok((_socket, _addr)) => {}
+                Err(_e) => {}
             }
         });
 
@@ -291,8 +331,14 @@ mod tests {
         test_servers.insert(FromStr::from_str("127.0.0.1:8082").unwrap(), Some(20));
         let test_bck = Arc::new(Backend::new("test".to_string(), test_servers, 1000));
 
-        let svr = get_next(test_bck);
-        assert_eq!(svr, Some(FromStr::from_str("127.0.0.1:8082").unwrap()));
+        let mut rt = runtime::Builder::new()
+            .basic_scheduler()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        let srv = rt.block_on(get_next(test_bck));
+        assert_eq!(srv, Some(FromStr::from_str("127.0.0.1:8082").unwrap()));
     }
 
     #[test]
@@ -300,29 +346,39 @@ mod tests {
         let mut test_servers = HashMap::new();
         let test_addr = FromStr::from_str("127.0.0.1:8089").unwrap();
         test_servers.insert(test_addr, None);
-
+        let mut rt = runtime::Builder::new()
+            .basic_scheduler()
+            .enable_all()
+            .build()
+            .unwrap();
         // not listening on 127.0.0.1:8089 yet so should be marked as unhealthy
         let test_bck = Backend::new("dummy".to_string(), test_servers.clone(), 1000);
         {
-            let test_srv_pool = test_bck.servers.read().unwrap();
-            assert_eq!(test_srv_pool.servers_map.get(&test_addr).unwrap().healthy, false);
+            let test_srv_pool = rt.block_on(test_bck.servers.read());
+            //let test_srv_pool = test_bck.servers.read().unwrap();
+            assert_eq!(
+                test_srv_pool.servers_map.get(&test_addr).unwrap().healthy,
+                false
+            );
         }
-
         // start listening on 127.0.0.1:8089 so health checks will mark as healthy
-        thread::spawn( ||{
+        thread::spawn(|| {
             let listener = TcpListener::bind("127.0.0.1:8089").unwrap();
             match listener.accept() {
-                Ok((_socket, _addr)) => {},
-                Err(_e) => {},
+                Ok((_socket, _addr)) => {}
+                Err(_e) => {}
             }
         });
         let one_sec = time::Duration::from_secs(1);
         thread::sleep(one_sec);
-
         let test_bck = Backend::new("dummy".to_string(), test_servers, 1000);
         {
-            let test_srv_pool = test_bck.servers.read().unwrap();
-            assert_eq!(test_srv_pool.servers_map.get(&test_addr).unwrap().healthy, true);
+            let test_srv_pool = rt.block_on(test_bck.servers.read());
+            //let test_srv_pool = test_bck.servers.read().unwrap();
+            assert_eq!(
+                test_srv_pool.servers_map.get(&test_addr).unwrap().healthy,
+                true
+            );
         }
     }
 }
